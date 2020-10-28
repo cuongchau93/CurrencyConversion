83fbeea05088423a15543e70c10bf922852739b5
RyanC <ts-cuong.chau@rakuten.com>
2020-10-28 14:57:19 +0900

    Initial Commit

---
+++ SimpleSwiftApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleSwiftApp
+//
+//  Created by Chau, Cuong | DCMS on 2020/10/28.
+//  Copyright © 2020 Chau, Cuong | DCMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
